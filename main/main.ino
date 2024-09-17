081f576cdf55a254f1c077f0abc141164bdc319c
guillaume-genois <guillaumegenois@yahoo.ca>
2024-09-16 22:33:38 -0400

    ajout connection wifi

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,7 @@
 //TEST CODE
 
+#include "RTC.h"
+#include <WiFiS3.h>
 
 #include <SoftwareSerial.h>
 
@@ -7,11 +9,28 @@ int trigPin = 2;
 int echoPin = 4;
 long duration, cm, inches;
 
+
+int testHour = 12, timeUpdateHour = 9;
+bool testSent = false, timeUpdated = false;
+bool sendMessage = true; // send a notification on powerup. 
+unsigned long unixTime;
+unsigned long unixTimeLastSend = 0;
+unsigned long minutesSinceLastSend;
+unsigned long minMinutesBetweenSends = 15;
+
+WiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
+
+#define WIFI_SSID "genois"
+#define WIFI_PASS "master13" // not needed with MAC address filtering
+
+
 void setup() {
   
   Serial.begin (9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  connectwifi();
+  WiFi.disconnect();
   delay(5000);
   Serial.println("Patrol Mode Initiated...");
 }
@@ -29,11 +48,25 @@ void loop() {
 
   inches = (duration / 2) / 74;
   Serial.println(inches);
+  /*
   if (inches < 20 || inches > 500) {
     Serial.println("Intruder Detected!");
     Serial.println("Sending text Notification...");
     delay(5000);
     Serial.println("Patrol Mode Initiated...");
   }
+  */
 
+}
+
+void connectwifi(){
+  Serial.begin(9600);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);  // WiFi.begin(WIFI_SSID) or WiFi.begin(WIFI_SSID, WIFI_PASS); depending on if password is necessary
+  Serial.print("Connecting to Wi-Fi.");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.print(" Connected with IP: ");
+  Serial.println(WiFi.localIP());
 }
