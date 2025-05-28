2d98485da70623823b4164647d5e876ea0d7b317
Guigui031 <guillaume.genois@umontreal.ca>
2025-05-28 16:27:39 -0400

    ajout enregistrement données photoresistor et ultrasound on sd card et command sur telegram

--- main/main.ino
+++ main/main.ino
@@ -1,4 +1,3 @@
-
 #include "WiFi.h"
 #include <WiFiClientSecure.h>
 #include <UniversalTelegramBot.h>
@@ -12,13 +11,16 @@ int sck = 18;
 int miso = 19;
 int mosi = 23;
 int cs = 5;
+const char* parking_data_folder = "/parking_data";
+const char* light_data_folder = "/light_data";
 
 // distance sensor
 int trigPin = 2;
 int echoPin = 4;
 long duration, cm, inches;
-long lastTime = millis();
+long lastEventTime = millis();
 long timerDelay = 15 * 1000;
+bool carPresent = false;
 
 // WiFi
 #define WIFI_SSID "SM-S921W6681"
@@ -32,11 +34,27 @@ String bot_name = "AuxVoletsNoirs_notifications"; // you can change this to whic
 WiFiClientSecure secured_client;
 UniversalTelegramBot bot(BOTtoken, secured_client);
 
+const unsigned long BOT_MTBS = 1000; // mean time between scan messages
+unsigned long bot_lasttime; // last time messages' scan has been done
+bool notificationsEnabled = true;
+
 // real time clock
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = -5 * 60 * 60;
 const int   daylightOffset_sec = 3600;
-int counter = 0;
+int event_counter = 0;
+struct tm timeinfo;
+char datestr[20];       // e.g. "2025-05-27"
+char timestr[10];       // e.g. "23:12:15"
+
+// Photoresistor
+const int ldrPin = 33;
+int lightValue;
+int log_counter = 0;
+unsigned long lastLogTime = 0;
+const unsigned long logInterval = 30000; // 1 hour in milliseconds = 3600000UL
+char dateDatastr[20];       // e.g. "2025-05-27"
+char timeDatastr[10];       // e.g. "23:12:15"
 
 
 void setup() {
@@ -50,12 +68,7 @@ void setup() {
   pinMode(echoPin, INPUT);
 
   // init pins for sd card module
-  SPI.begin(sck, miso, mosi, cs);
-  if (!SD.begin(cs)) {
-    Serial.println("SD Card Mount Failed");
-    return;
-  }
-  Serial.println("SD Card Mount Succeded");
+  connectSDCardModule();
 
   // Init time
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
@@ -63,29 +76,76 @@ void setup() {
   // put your setup code here, to run once:
   connectwifi();
   connectTelegramBot();
+  lastLogTime = millis();
+  measureData(log_counter++, false);
   
   Serial.println("Patrol Mode Initiated...");
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
   // put your main code here, to run repeatedly:
   digitalWrite(trigPin, LOW);
-  delayMicroseconds(1);
-  digitalWrite(trigPin, HIGH);
   delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(5);
   digitalWrite(trigPin, LOW);
 
   pinMode(echoPin, INPUT);
   duration = pulseIn(echoPin, HIGH);
 
   inches = (duration / 2) / 74;
-  Serial.println(inches);
+  cm = (duration / 2) / 29;
+  Serial.println(cm);
 
-  if ((millis() - lastTime) > timerDelay && inches < 10 && inches > 0) {
-    bot.sendMessage(Mychat_id, "Une auto a traversé!", "Markdown");
-    lastTime = millis();
-    logEvent(counter++);
-  } 
+  // Detection logic
+  // if (inches < 10 && inches > 0) {
+  //   // Car detected
+  //   if (!carPresent) {
+  //     Serial.println("Car entered.");
+  //     carPresent = true;
+  //   }
+  // } else if (inches > 0) {
+  //   // Car not detected
+  //   if (carPresent) {
+  //     Serial.println("Car left.");
+  //     if (notificationsEnabled) {
+  //       bot.sendMessage(Mychat_id, "Une auto a traversé!", "Markdown");
+  //     }
+  //     logEvent(event_counter++);
+  //     carPresent = false;
+  //   }
+  // }
+  if (cm < 30 && cm > 0  && currentMillis - lastEventTime > timerDelay) {
+      if (notificationsEnabled) {
+        bot.sendMessage(Mychat_id, "Une auto a traversé!", "Markdown");
+      }
+      logEvent(event_counter++);
+      lastEventTime = currentMillis;
+  }
+
+  // Measure data
+  if (currentMillis - lastLogTime >= logInterval) {
+    lastLogTime = currentMillis;
+    measureData(log_counter++, false);
+  }
+
+  if (currentMillis - bot_lasttime > BOT_MTBS) {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    while (numNewMessages) {
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    bot_lasttime = currentMillis;
+  }
+}
+
+void measureData(int log_counter, bool from_command) {
+  lightValue = analogRead(ldrPin); // Range: 0 (dark) to 1023 (bright)
+  Serial.print("Light level: ");
+  Serial.println(lightValue);
+  logLightValue(log_counter, from_command);
 }
 
 
@@ -106,7 +166,7 @@ void connectTelegramBot() {
 
   if (WiFi.status() == WL_CONNECTED) {
     Serial.println("WiFi connected, attempting to send Telegram message...");
-    bool success = bot.sendMessage(Mychat_id, "Bot connecté!", "");
+    bool success = bot.sendMessage(Mychat_id, "Bot connecté! \n/help pour afficher les commandes disponibles.", "");
     if (!success) {
       Serial.println("Failed to send message.");
     }
@@ -115,27 +175,61 @@ void connectTelegramBot() {
   }
 }
 
+void connectSDCardModule() {
+  SPI.begin(sck, miso, mosi, cs);
+  if (!SD.begin(cs)) {
+    Serial.println("SD Card Mount Failed");
+    return;
+  }
+  Serial.println("SD Card Mount Succeeded");
+
+  // Ensure parking_data folder exists
+  if (!SD.exists(parking_data_folder)) {
+    Serial.printf("Folder %s does not exist. Creating...\n", parking_data_folder);
+    if (SD.mkdir(parking_data_folder)) {
+      Serial.println("Folder created successfully.");
+    } else {
+      Serial.println("Failed to create folder.");
+    }
+  } else {
+    Serial.printf("Folder %s already exists.\n", parking_data_folder);
+  }
+  
+  // Ensure light_data folder exists
+  if (!SD.exists(light_data_folder)) {
+    Serial.printf("Folder %s does not exist. Creating...\n", light_data_folder);
+    if (SD.mkdir(light_data_folder)) {
+      Serial.println("Folder created successfully.");
+    } else {
+      Serial.println("Failed to create folder.");
+    }
+  } else {
+    Serial.printf("Folder %s already exists.\n", light_data_folder);
+  }
+}
+
 void logEvent(int value) {
-  struct tm timeinfo;
   if (!getLocalTime(&timeinfo)) {
     Serial.println("Failed to get time");
     return;  //TODO: enregistrer autre chose au lieu
   }
 
   // Create filename: /YYYY-MM-DD.csv
-  char filename[20];
-  strftime(filename, sizeof(filename), "/%Y-%m-%d.csv", &timeinfo);
+  char filename[40];      // File path string
+  strftime(datestr, sizeof(datestr), "%Y-%m-%d", &timeinfo);
+  snprintf(filename, sizeof(filename), "%s/%s.csv", parking_data_folder, datestr);
+  Serial.print("Creating/using file: ");
+  Serial.println(filename);
 
   // Create header if file doesn't exist
   if (!SD.exists(filename)) {
-    writeFile(SD, filename, "index,distance,time,millis\n"); // CSV header
+    writeFile(SD, filename, "index,date,time,distance(cm),millis,notification\n"); // CSV header
   }
 
   // Create data line: value,HH:MM:SS\n
-  char line[32];
-  char timestr[10];
+  char line[64];
   strftime(timestr, sizeof(timestr), "%H:%M:%S", &timeinfo);
-  snprintf(line, sizeof(line), "%d,%d,%s,%d\n", value, inches, timestr, lastTime);
+  snprintf(line, sizeof(line), "%d,%s,%s,%d,%d,%d\n", value, datestr, timestr, cm, lastEventTime, notificationsEnabled);
 
   // Append to file
   appendFile(SD, filename, line);
@@ -143,6 +237,82 @@ void logEvent(int value) {
   // Print to Serial
   Serial.print("Logged: ");
   Serial.print(line);
+}
+
+void logLightValue(int value, bool from_command) {
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to get time");
+    return;  //TODO: enregistrer autre chose au lieu
+  }
+
+  // Create filename: /YYYY-MM-DD.csv
+  char filename[40];      // File path string
+  strftime(dateDatastr, sizeof(dateDatastr), "%Y-%m-%d", &timeinfo);
+  snprintf(filename, sizeof(filename), "%s/%s.csv", light_data_folder, dateDatastr);
+  Serial.print("Creating/using file: ");
+  Serial.println(filename);
+
+  // Create header if file doesn't exist
+  if (!SD.exists(filename)) {
+    writeFile(SD, filename, "index,date,time,light,millis,command\n"); // CSV header
+  }
+
+  // Create data line
+  char line[64];
+  strftime(timeDatastr, sizeof(timeDatastr), "%H:%M:%S", &timeinfo);
+  snprintf(line, sizeof(line), "%d,%s,%s,%d,%d,%d\n", value, dateDatastr, timeDatastr, lightValue, lastLogTime, from_command);
+
+  // Append to file
+  appendFile(SD, filename, line);
+
+  // Print to Serial
+  Serial.print("Logged: ");
+  Serial.print(line);
+}
+
+void handleNewMessages(int numNewMessages)
+{
+  for (int i = 0; i < numNewMessages; i++)
+  {
+    String chat_id = bot.messages[i].chat_id;
+    String text = bot.messages[i].text;
+    bot.sendMessage(chat_id, text, "");
+    if (text == "/status") {
+      bot.sendMessage(chat_id, "Système connecté et en marche.", "");
+    } else if (text == "/log") {
+      bot.sendMessage(chat_id, 
+        "Dernière auto détectée : " + String(timestr) + ".\n"
+        "Dernier enregistrement de données : " + String(timeDatastr) + ".\n"
+        "Dernière intensité lumineuse enregistrée : " + String(lightValue) + "."
+        , ""
+      );
+    } else if (text == "/data") {
+      measureData(log_counter++, true);
+      bot.sendMessage(chat_id, 
+        "Présente intensité lumineuse : " + String(lightValue) + "."
+        , ""
+      );
+    } else if (text == "/off") {
+      notificationsEnabled = false;
+      bot.sendMessage(chat_id, "Notifications désactivées.", "Markdown");
+    } else if (text == "/on") {
+      notificationsEnabled = true;
+      bot.sendMessage(chat_id, "Notifications activées.", "Markdown");
+    } else if (text == "/help") {
+      bot.sendMessage(chat_id,
+        "*Commandes disponibles:*\n"
+        "/status - Affiche l'état du système\n"
+        "/log - Affiche l'heure de la dernière auto détectée\n"
+        "/data - Affiche les valeurs des différentes sondes présentement\n"
+        "/on - Active les notifications lors d'une détection d'auto\n"
+        "/off - Désactive les notifications jusqu'à réactivation avec /on. Enregistre tout de même les détections\n"
+        "/help - Affiche cette aide\n"
+        , "Markdown"
+      );
+    } else {
+      bot.sendMessage(chat_id, "Commande inconnue. Tapez /help pour la liste des commandes.", "");
+    }
+  }
 }
 
 void listDir(fs::FS &fs, const char *dirname, uint8_t levels) {
