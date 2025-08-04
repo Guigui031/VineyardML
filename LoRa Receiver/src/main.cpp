8899d2af0fe514892bb780c57805502e5d29ec6f
Guigui031 <guillaume.genois@umontreal.ca>
2025-08-04 13:06:52 -0400

    adding core functions

--- LoRa Receiver/src/main.cpp
+++ LoRa Receiver/src/main.cpp
@@ -9,6 +9,12 @@
 #define HELTEC_POWER_BUTTON   // must be before "#include <heltec_unofficial.h>"
 #include <Arduino.h>
 #include <heltec_unofficial.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+#include "time.h"
 
 // Frequency in MHz - MUST match transmitter frequency
 #define FREQUENCY           866.3       // for Europe
@@ -172,6 +178,77 @@ void logReceivedData(String data) {
   */
 }
 
+void connectwifi(){
+  WiFi.begin(WIFI_SSID, WIFI_PASS);  // WiFi.begin(WIFI_SSID) or WiFi.begin(WIFI_SSID, WIFI_PASS); depending on if password is necessary
+  Serial.print("Connecting to Wi-Fi:");
+  Serial.print(WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.print(" Connected with IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void connectTelegramBot() {
+  secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("WiFi connected, attempting to send Telegram message...");
+    bool success = bot.sendMessage(Mychat_id, "Bot connecté! \n/help pour afficher les commandes disponibles.", "");
+    if (!success) {
+      Serial.println("Failed to send message.");
+    }
+  } else {
+    Serial.println("WiFi not connected.");
+  }
+}
+
+void handleNewMessages(int numNewMessages)
+{
+  for (int i = 0; i < numNewMessages; i++)
+  {
+    String chat_id = bot.messages[i].chat_id;
+    String text = bot.messages[i].text;
+    bot.sendMessage(chat_id, text, "");
+    if (text == "/status") {
+      bot.sendMessage(chat_id, "Système connecté et en marche.", "");
+    } else if (text == "/log") {
+      bot.sendMessage(chat_id, 
+        "Dernière auto détectée : " + String(timestr) + ".\n"
+        "Dernier enregistrement de données : " + String(timeDatastr) + ".\n"
+        "Dernière intensité lumineuse enregistrée : " + String(lightValue) + "."
+        , ""
+      );
+    } else if (text == "/data") {
+      measureData(log_counter++, true);
+      bot.sendMessage(chat_id, 
+        "Présente intensité lumineuse : " + String(lightValue) + "."
+        , ""
+      );
+    } else if (text == "/off") {
+      notificationsEnabled = false;
+      bot.sendMessage(chat_id, "Notifications désactivées.", "Markdown");
+    } else if (text == "/on") {
+      notificationsEnabled = true;
+      bot.sendMessage(chat_id, "Notifications activées.", "Markdown");
+    } else if (text == "/help") {
+      bot.sendMessage(chat_id,
+        "*Commandes disponibles:*\n"
+        "/status - Affiche l'état du système\n"
+        "/log - Affiche l'heure de la dernière auto détectée\n"
+        "/data - Affiche les valeurs des différentes sondes présentement\n"
+        "/on - Active les notifications lors d'une détection d'auto\n"
+        "/off - Désactive les notifications jusqu'à réactivation avec /on. Enregistre tout de même les détections\n"
+        "/help - Affiche cette aide\n"
+        , "Markdown"
+      );
+    } else {
+      bot.sendMessage(chat_id, "Commande inconnue. Tapez /help pour la liste des commandes.", "");
+    }
+  }
+}
+
 void setup() {  
   heltec_setup();
   
@@ -214,6 +291,9 @@ void setup() {
   display.drawString(0, 40, "Packets: 0");
   display.display();
   
+  connectwifi();
+  connectTelegramBot();
+
   Serial.println("Setup complete. Listening for packets...");
 }
 
